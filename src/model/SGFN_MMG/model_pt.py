33f9b160beb5de159e9c672757598fe46161f479
Kunho Heo <93777460+Heokunho@users.noreply.github.com>
2025-05-14 11:24:22 +0900

    Feat: diff

--- src/model/SGFN_MMG/model_pt.py
+++ src/model/SGFN_MMG/model_pt.py
@@ -408,7 +408,7 @@ class Mmgnet(BaseModel):
         # compute triplet loss
         # triplet_loss = self.compute_triplet_loss(obj_logits_3d, rel_cls_3d, obj_logits_2d, rel_cls_2d, edge_indices)
                
-        loss = lambda_o * (loss_obj_3d) + 3.0 * lambda_r * (loss_rel_3d) + 0.1 * (rel_mimic_3d) # + rel_diff
+        loss = lambda_o * (loss_obj_3d) + 3.0 * lambda_r * (loss_rel_3d) + 0.1 * (rel_mimic_3d) + 0.1 * rel_diff
         #loss = lambda_o * (loss_obj_2d + loss_obj_3d) + 3 * lambda_r * (loss_rel_2d + loss_rel_3d) + 0.1 * (loss_mimic + rel_mimic_2d)
         self.backward(loss)
         
