9cde8a4152f2aabc6045a3f86460794b5e14efb6
Kunho Heo <93777460+Heokunho@users.noreply.github.com>
2025-05-14 11:27:29 +0900

    Feat: no film

--- src/model/SGFN_MMG/model_pt.py
+++ src/model/SGFN_MMG/model_pt.py
@@ -59,20 +59,20 @@ class Mmgnet(BaseModel):
         self.obj_encoder.load_state_dict(torch.load(self.mconfig.obj_pretrian_path))
         self.obj_encoder = self.obj_encoder.eval()
         
-        # self.rel_encoder_3d = RelFeatNaiveExtractor(
-        #     dim_point_feature,
-        #     dim_descriptor,
-        #     dim_edge_feature,
-        #     num_layers=8
-        # ) #.to("cuda")
+        self.rel_encoder_3d = RelFeatNaiveExtractor(
+            dim_point_feature,
+            dim_descriptor,
+            dim_edge_feature,
+            num_layers=8
+        ) #.to("cuda")
 
-        self.rel_encoder_3d = GeocentricRelationEncoder(
-            input_dim=dim_point_feature,
-            geo_dim=dim_descriptor,
-            hidden_dim=256,
-            geo_factor=4,
-            num_layers=2
-        )
+        # self.rel_encoder_3d = GeocentricRelationEncoder(
+        #     input_dim=dim_point_feature,
+        #     geo_dim=dim_descriptor,
+        #     hidden_dim=256,
+        #     geo_factor=4,
+        #     num_layers=2
+        # )
 
         # self.rel_encoder_3d = PointNetfeat(
         #     global_feat=True,
