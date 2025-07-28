f43fe5960842f2f729bdeefd2549c6889b8c6cd5
spoiuy3 <104378473+spoiuy3@users.noreply.github.com>
2025-07-28 12:22:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ def load_config():
     20250725_003841_front_gat
     20250725_003417_front_gat
     '''
-    config.exp = "20250725_005953_2029"
+    config.exp = "20250725_010006_2029"
     
     print(f"exp name: {config.exp}")
     
